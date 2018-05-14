3c7ff6a56ca5369a87d02c13245b14a84aa29450
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-05-14 16:56:48 +0100

    first commit

---
+++ MyPythonPlayGround.py
@@ -0,0 +1,24 @@
+class MyPythonPlayGround:
+    # Simple Python Playground
+    #
+
+    # output to console
+    print("Hello Arty")
+
+
+    # method definition
+    def meth():
+        print("I'm in a method")
+
+
+    # None = because the method has not return Type
+    print(meth())
+
+
+    # Method with param
+    def printName(x):
+        return "Hello " + x
+
+
+    # Pass name as value.
+    print(printName("Artemas"))
