35ad395a9c6b0df40a6827dbf779b89f89ce227a
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-05-21 10:21:56 +0100

    add If Statement topic

--- MyPythonPlayGround.py
+++ MyPythonPlayGround.py
@@ -37,3 +37,32 @@ class MyPythonPlayGround:
     print(str(python_course))  # "True"
 
     aliens_found = None  # None == null in other languages.
+
+    # If Statements
+    number = 5
+    if number == 5:
+        print("Number is 5")
+    else:
+        print("Number is NOT 5")
+
+    # Truthy and Falsy Values
+    number = 5
+    if number:
+        print("Number is defined and truthy")
+
+    text = "Python"
+    if text:
+        print("Text is defined and truthy")
+
+    number = 3
+    python_course = True
+    if number == 3 and python_course:
+        print("This will execute")
+
+    if number == 17 or python_course:
+        print("This will also execute")
+
+    # Ternary If Statements
+    a = 1
+    b = 2
+    print("bigger" if a > b else "smaller")
